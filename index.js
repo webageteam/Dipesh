07ddf071ca49cbb1c1aed4700f78125737c1834d
PatelDipesh211 <93267660+PatelDipesh211@users.noreply.github.com>
2022-04-04 18:52:51 +0530

    waste file delete

--- index.js
+++
@@ -1,9 +0,0 @@
-const buttonRight = document.getElementById('slideRight');
-const buttonLeft = document.getElementById('slideLeft');
-
-buttonRight.onclick = function () {
-  document.getElementById('container').scrollLeft += 900;
-};
-buttonLeft.onclick = function () {
-  document.getElementById('container').scrollLeft -= 900;
-};
